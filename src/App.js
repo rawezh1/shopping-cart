feeaa9e8dccb31e2ba05f620e1dd7a1370764fbc
Rawezh <rawezhmarif@gmail.com>
2021-07-08 21:34:25 +0300

    setup shop page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,63 @@
 import React, { useState, useEffect } from 'react';
 import NavBar from './components/nav-bar';
 import Home from './components/homepage';
+import Shop from './components/shop';
+import Beetroot from './components/images/beetroot.webp';
+import Classic from './components/images/classic.jpg';
+import Avocado from './components/images/avocado.jpg';
+import Potato from './components/images/potato.webp';
 import './App.css';
 
 function App() {
+  const classic = {
+    src: Classic,
+    id: 'classic',
+    name: 'Classic Hummus',
+    price: '$1',
+    weight: '100 g',
+    ingredients: 'Chickpeas, Salt, Olive Oil, Garlic, Tahini, Spices',
+  };
+  const beetroot = {
+    src: Beetroot,
+    id: 'beetroot',
+    name: 'Beetroot Hummus',
+    price: '$2',
+    weight: '100 g',
+    ingredients: 'Chickpeas, Beetroot, Salt, Tahini, Spices',
+  };
+  const avocado = {
+    src: Avocado,
+    id: 'avocado',
+    name: 'Avocado Hummus',
+    price: '$5',
+    weight: '100 g',
+    ingredients: 'Chickpeas, Avocado, Salt, Tahini, Spices',
+  };
+  const potato = {
+    src: Potato,
+    id: 'potato',
+    name: 'Sweet Potato Hummus',
+    price: '$3',
+    weight: '100 g',
+    ingredients: 'Chickpeas, Sweet Potato, Salt, Tahini, Spices',
+  };
+  const cardInfo = [classic, beetroot, potato, avocado];
+  const cartInit = {
+    calssic: 0,
+    beetroot: 0,
+    potato: 0,
+    avocado: 0,
+  };
+  const [cart, setCart] = useState(cartInit);
+  const addItem = (e) => {
+    let newCart = cart;
+    newCart[e.target.id] += 1
+    setCart(newCart);
+  };
   return (
     <div className='App'>
-      <NavBar/>
-      <Home />
+      <NavBar />
+      <Shop clickHandler={addItem} cardInfo={cardInfo} />
     </div>
   );
 }
--- src/components/shop.js
+++ src/components/shop.js
@@ -1,14 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import './shop.css'
+import './shop.css';
 
-
-function Shop() {
-  return (
-    <div className='home'>
-        <h2>Welcome</h2>
-        <p>Description</p>
-    </div>
-  );
+function Shop(props) {
+  const makeCards = () => {
+    return props.cardInfo.map((card, index) => {
+      return (
+        <div key={index} className='card'>
+          <h1 className='card-title'>{card.name}</h1>
+          <img className='card-img' src={card.src} alt=''>
+          </img>
+          <p className='card-text'>Price: {card.price}</p>
+          <p className='card-text'>Weight: {card.weight}</p>
+          <p className='card-text'>Ingredients: {card.ingredients}</p>
+          <button id = {card.id} className='card-button' onClick={props.clickHandler}>Add to cart</button>
+        </div>
+      );
+    });
+  };
+  return <div className='shop'>{makeCards()}</div>;
 }
 
 export default Shop;
