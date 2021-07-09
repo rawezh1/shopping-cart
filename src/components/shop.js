64846fe4da11b5694f5a254b3e6b118f60f65a19
Rawezh <rawezhmarif@gmail.com>
2021-07-09 14:46:01 +0300

    shop styling improvement

--- src/components/shop.js
+++ src/components/shop.js
@@ -5,14 +5,25 @@ function Shop(props) {
   const makeCards = () => {
     return props.cardInfo.map((card, index) => {
       return (
-        <div key={index} className='card' style={{backgroundImage:`${card.src}`}}>
-          <h1 className='card-title'>{card.name}</h1>
-          <img className='card-img' src={card.src} alt=''>
-          </img>
-          <p className='card-text'>Price: {card.price}</p>
-          <p className='card-text'>Weight: {card.weight}</p>
-          <p className='card-text'>Ingredients: {card.ingredients}</p>
-          <button id = {card.id} className='card-button' onClick={props.clickHandler}>Add to cart</button>
+        <div
+          key={index}
+          className='card'
+          style={{ backgroundImage: `url(${card.src})` }}
+        >
+          <div className='card-cover'>
+            <h1 className='card-title'>{card.name}</h1>
+
+            <p className='card-text'>Price: {card.price}</p>
+            <p className='card-text'>Weight: {card.weight}</p>
+            <p className='card-text'>Ingredients: {card.ingredients}</p>
+            <button
+              id={card.id}
+              className='card-button'
+              onClick={props.clickHandler}
+            >
+              Add to cart
+            </button>
+          </div>
         </div>
       );
     });
